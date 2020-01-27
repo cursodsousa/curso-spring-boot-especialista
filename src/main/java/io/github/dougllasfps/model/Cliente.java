c3ea8ccafd22000351f20b94fb331031cfef4dbf
Dougllas Sousa <dougllasfps@gmail.com>
2020-01-26 21:00:37 -0300

    injeção de dependências

---
+++ src/main/java/io/github/dougllasfps/model/Cliente.java
@@ -0,0 +1,13 @@
+package io.github.dougllasfps.model;
+
+public class Cliente {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/io/github/dougllasfps/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.github.dougllasfps.repository;
+
+import io.github.dougllasfps.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        //acessa a base e salvar o cliente
+    }
+}
