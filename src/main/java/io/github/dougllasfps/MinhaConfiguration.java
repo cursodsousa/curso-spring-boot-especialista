a741c98d564b3e010efd081eb43552fdc2840d3a
Dougllas Sousa <dougllasfps@gmail.com>
2020-01-28 18:57:12 -0300

    injeção de dependências

--- src/main/java/io/github/dougllasfps/MinhaConfiguration.java
+++ src/main/java/io/github/dougllasfps/MinhaConfiguration.java
@@ -1,10 +1,18 @@
 package io.github.dougllasfps;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
-
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 }
