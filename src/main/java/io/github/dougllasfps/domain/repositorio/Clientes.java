e886e2f409f41aac87eeec48018eb899231425ba
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-02 15:37:23 -0300

    concluindo crud de clientes jdbc

--- src/main/java/io/github/dougllasfps/domain/repositorio/Clientes.java
+++ src/main/java/io/github/dougllasfps/domain/repositorio/Clientes.java
@@ -15,6 +15,8 @@ public class Clientes {
 
     private static String INSERT = "insert into cliente (nome) values (?) ";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
+    private static String UPDATE = "update cliente set nome = ? where id = ? ";
+    private static String DELETE = "delete from cliente where id = ? ";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -24,15 +26,40 @@ public class Clientes {
         return cliente;
     }
 
+    public Cliente atualizar(Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(), cliente.getId()} );
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> buscarPorNome(String nome){
+        return jdbcTemplate.query(
+                SELECT_ALL.concat(" where nome like ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+    }
+
     public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 Integer id = resultSet.getInt("id");
                 String nome = resultSet.getString("nome");
                 return new Cliente(id, nome);
             }
-        });
+        };
     }
 
 }
