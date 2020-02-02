5d5761d73f076da8c85054ce61ec8f47a6f7fcd3
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-02 16:01:12 -0300

    Persistindo entidades com entityManager

--- src/main/java/io/github/dougllasfps/domain/repositorio/Clientes.java
+++ src/main/java/io/github/dougllasfps/domain/repositorio/Clientes.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -13,7 +15,6 @@ import java.util.List;
 @Repository
 public class Clientes {
 
-    private static String INSERT = "insert into cliente (nome) values (?) ";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
     private static String UPDATE = "update cliente set nome = ? where id = ? ";
     private static String DELETE = "delete from cliente where id = ? ";
@@ -21,8 +22,12 @@ public class Clientes {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
     public Cliente salvar(Cliente cliente){
-        jdbcTemplate.update( INSERT, new Object[]{cliente.getNome()} );
+        entityManager.persist(cliente);
         return cliente;
     }
 
