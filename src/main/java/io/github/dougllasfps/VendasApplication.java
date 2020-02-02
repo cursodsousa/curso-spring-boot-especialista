8f5c34d481a9b3851ace643aa7cbe3184039f454
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-02 11:53:21 -0300

    configurando conexão com banco de dados

--- src/main/java/io/github/dougllasfps/VendasApplication.java
+++ src/main/java/io/github/dougllasfps/VendasApplication.java
@@ -1,24 +1,10 @@
 package io.github.dougllasfps;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
-
-    @Value("${application.name}")
-    private String applicationName;
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return applicationName;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
