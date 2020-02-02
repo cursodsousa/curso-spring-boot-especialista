8b38323cdc91227a1e4ba605d3fc257bb204dbd3
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-02 15:53:54 -0300

    Mapeando a entidade cliente JPA

--- src/main/java/io/github/dougllasfps/domain/entity/Cliente.java
+++ src/main/java/io/github/dougllasfps/domain/entity/Cliente.java
@@ -1,8 +1,17 @@
 package io.github.dougllasfps.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table( name = "cliente" )
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "nome", length = 100)
     private String nome;
 
     public Cliente() {
