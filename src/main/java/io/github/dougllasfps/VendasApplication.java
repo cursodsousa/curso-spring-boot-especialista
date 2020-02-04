93e907a4dd6497d0eff983b255a819edfb8d7fec
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-03 23:17:04 -0300

    Query Methods

--- src/main/java/io/github/dougllasfps/VendasApplication.java
+++ src/main/java/io/github/dougllasfps/VendasApplication.java
@@ -17,35 +17,13 @@ public class VendasApplication {
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
             System.out.println("Salvando clientes");
-            clientes.salvar(new Cliente("Dougllas"));
-            clientes.salvar(new Cliente("Outro Cliente"));
+            clientes.save(new Cliente("Fulano"));
+            clientes.save(new Cliente("Outro Cliente"));
 
-            List<Cliente> todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
+            boolean existe = clientes.existsByNome("Dougllas");
+            System.out.println("existe um cliente com o nome Dougllas? " + existe);
 
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.atualizar(c);
-            });
 
-            todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Buscando clientes");
-            clientes.buscarPorNome("Cli").forEach(System.out::println);
-
-            System.out.println("deletando clientes");
-            clientes.obterTodos().forEach(c -> {
-                clientes.deletar(c);
-            });
-
-            todosClientes = clientes.obterTodos();
-            if(todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado.");
-            }else{
-                todosClientes.forEach(System.out::println);
-            }
         };
     }
 
--- src/main/java/io/github/dougllasfps/domain/repositorio/Clientes.java
+++ src/main/java/io/github/dougllasfps/domain/repositorio/Clientes.java
@@ -1,63 +1,14 @@
 package io.github.dougllasfps.domain.repositorio;
 
 import io.github.dougllasfps.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-@Repository
-public class Clientes {
+public interface Clientes extends JpaRepository<Cliente, Integer > {
 
-    @Autowired
-    private EntityManager entityManager;
+    List<Cliente> findByNomeLike(String nome);
 
-    @Transactional
-    public Cliente salvar(Cliente cliente){
-        entityManager.persist(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public Cliente atualizar(Cliente cliente){
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente){
-        if(!entityManager.contains(cliente)){
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletar(Integer id){
-        Cliente cliente = entityManager.find( Cliente.class, id );
-        deletar(cliente);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome){
-       String jpql = " select c from Cliente c where c.nome like :nome ";
-       TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-       query.setParameter("nome", "%" + nome +"%");
-       return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> obterTodos(){
-        return entityManager
-                .createQuery("from Cliente", Cliente.class)
-                .getResultList();
-    }
+    boolean existsByNome(String nome);
 
 }
