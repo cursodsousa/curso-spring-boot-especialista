6f76da0c01274dfc757f0dbf924e787a71da38ee
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-22 16:07:19 -0300

    Requisicão Delete

--- src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
+++ src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
@@ -39,4 +39,17 @@ public class ClienteController {
         return ResponseEntity.ok(clienteSalvo);
     }
 
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete( @PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            clientes.delete( cliente.get() );
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
