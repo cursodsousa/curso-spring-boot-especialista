e62f3e12c299c2a12bf48dd5e240750f8ec72ea0
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-22 11:33:41 -0300

    request mapping

--- src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
+++ src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
@@ -1,17 +1,21 @@
 package io.github.dougllasfps.rest.controller;
 
+import io.github.dougllasfps.domain.entity.Cliente;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ClienteController {
 
-    @RequestMapping(value = "/api/clientes/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/api/clientes/hello/{nome}", "/api/hello"},
+            method = RequestMethod.POST,
+            consumes = { "application/json", "application/xml" },
+            produces = { "application/json", "application/xml" }
+    )
     @ResponseBody
-    public String helloCliente( @PathVariable("nome") String nomeCliente ){
+    public Cliente helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente){
         return String.format("Hello %s ", nomeCliente);
     }
 
