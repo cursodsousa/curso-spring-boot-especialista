dde19a3ed34d1c314721bead1288bcc1c6cff99a
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-22 12:01:51 -0300

    Requisição GET com parâmentros e Response Entity

--- src/main/java/io/github/dougllasfps/VendasApplication.java
+++ src/main/java/io/github/dougllasfps/VendasApplication.java
@@ -1,10 +1,23 @@
 package io.github.dougllasfps;
 
+import io.github.dougllasfps.domain.entity.Cliente;
+import io.github.dougllasfps.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+        return args -> {
+            Cliente c = new Cliente(null, "Fulano");
+            clientes.save(c);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
--- src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
+++ src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
@@ -1,22 +1,35 @@
 package io.github.dougllasfps.rest.controller;
 
 import io.github.dougllasfps.domain.entity.Cliente;
+import io.github.dougllasfps.domain.repository.Clientes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class ClienteController {
 
-    @RequestMapping(
-            value = {"/api/clientes/hello/{nome}", "/api/hello"},
-            method = RequestMethod.POST,
-            consumes = { "application/json", "application/xml" },
-            produces = { "application/json", "application/xml" }
-    )
+    private Clientes clientes;
+
+    public ClienteController( Clientes clientes ) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public Cliente helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente){
-        return String.format("Hello %s ", nomeCliente);
+    public ResponseEntity getClienteById( @PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok( cliente.get() );
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
