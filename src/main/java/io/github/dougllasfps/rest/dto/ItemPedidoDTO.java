0d56c08347a71fbae079da6e325b3e09b6304b11
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-23 23:04:21 -0300

    Trabalhando com o modelo DTO

---
+++ src/main/java/io/github/dougllasfps/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,6 @@
+package io.github.dougllasfps.rest.dto;
+
+public class ItemPedidoDTO {
+    private Integer produto;
+    private Integer quantidade;
+}
---
+++ src/main/java/io/github/dougllasfps/rest/dto/PedidoDTO.java
@@ -0,0 +1,10 @@
+package io.github.dougllasfps.rest.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class PedidoDTO {
+    private Integer cliente;
+    private BigDecimal total;
+    private List<ItemPedidoDTO> items;
+}
