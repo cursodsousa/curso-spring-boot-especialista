6438ee021c8ecbafacab9865bfb88baf9a19c274
Dougllas Sousa <dougllasfps@gmail.com>
2020-02-29 23:42:48 -0300

    Autenticação Basic

--- src/main/java/io/github/dougllasfps/config/SecurityConfig.java
+++ src/main/java/io/github/dougllasfps/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/produtos/**")
                     .hasRole("ADMIN")
             .and()
-                .formLogin();
+                .httpBasic();
         ;
     }
 
