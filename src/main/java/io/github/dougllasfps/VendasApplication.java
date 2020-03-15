d59323704e8e9f9262978af5d3ada06d0d024a6a
Dougllas Sousa <dougllasfps@gmail.com>
2020-03-15 16:17:26 -0300

    build utilizando multiplos profiles do maven

--- src/main/java/io/github/dougllasfps/VendasApplication.java
+++ src/main/java/io/github/dougllasfps/VendasApplication.java
@@ -2,9 +2,10 @@ package io.github.dougllasfps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
