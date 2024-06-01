238f80ac744314327dc80972270aeeebbae5ebbd
dougllas.sousa <dougllas.sousa@seduc.ce.gov.br>
2024-05-31 22:11:33 -0300

    [produtos-api] 09. Obtendo dados do produto e deletando

--- produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -2,11 +2,9 @@ package io.github.cursodsousa.produtosapi.controller;
 
 import io.github.cursodsousa.produtosapi.model.Produto;
 import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -28,5 +26,15 @@ public class ProdutoController {
 
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
     }
 }
