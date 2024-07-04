5a6d249a511f18d4d7558e16af54459d678bc2ec
dougllas.sousa <dougllas.sousa@seduc.ce.gov.br>
2024-07-03 21:55:57 -0300

    [jpa] criacao do projeto

---
+++ libraryapi/src/test/java/io/github/cursodsousa/libraryapi/LibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursodsousa.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
