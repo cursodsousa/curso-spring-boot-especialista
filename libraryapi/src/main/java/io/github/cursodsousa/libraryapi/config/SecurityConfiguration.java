f6eb9f098e1904174b23b9fd9730aca3f4feb6ec
dougllas.sousa <dougllas.sousa@seduc.ce.gov.br>
2024-10-26 11:55:46 -0300

    [actuator] adicionando e configurando o actuator

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/config/SecurityConfiguration.java
@@ -61,7 +61,8 @@ public class SecurityConfiguration {
                     "/swagger-resources/**",
                     "/swagger-ui.html",
                     "/swagger-ui/**",
-                    "/webjars/**"
+                    "/webjars/**",
+                    "/actuator/**"
             );
     }
 
