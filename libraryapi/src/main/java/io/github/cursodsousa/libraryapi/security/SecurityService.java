e4e397828231e62b18dda410195b570c0b4072f0
dougllas.sousa <dougllas.sousa@seduc.ce.gov.br>
2024-10-26 10:52:58 -0300

    [security] ajuste no security service

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/security/SecurityService.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/security/SecurityService.java
@@ -16,8 +16,11 @@ public class SecurityService {
 
     public Usuario obterUsuarioLogado(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String login = userDetails.getUsername();
-        return usuarioService.obterPorLogin(login);
+
+        if(authentication instanceof CustomAuthentication customAuth){
+            return customAuth.getUsuario();
+        }
+
+        return null;
     }
 }
